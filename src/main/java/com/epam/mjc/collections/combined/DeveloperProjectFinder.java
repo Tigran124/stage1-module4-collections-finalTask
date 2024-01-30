71f0578cb6de037ab4150e6aab9492856bdb247b
Tigran124 <tigran.h.988@gmail.com>
2024-01-30 15:22:23 +0400

    changes completed

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,31 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        projects.forEach((x, y) ->{
+            if (y.contains(developer)){
+                list.add(x);
+            }
+        });
+        Comparator<String> comparator = (o1, o2) -> {
+            if (o1.length() > o2.length()){
+                return -1;
+            } else if (o1.length() == o2.length()) {
+                if (o1.compareTo(o2) > 0){
+                    return -1;
+                } else if (o1.compareTo(o2) < 0) {
+                    return 1;
+                }else {
+                    return 0;
+                }
+            }else {
+                return 1;
+            }
+        };
+        list.sort(comparator);
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        sourceMap.forEach((x, y) ->{
+            if(map.containsKey(x.length())){
+                map.get(x.length()).add(x);
+            }else {
+                Set<String> set = new HashSet<>();
+                set.add(x);
+                map.put(x.length(),set);
+            }
+        });
+        return map;
     }
 }
